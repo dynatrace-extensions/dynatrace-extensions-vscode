1b41f8064b9c6e567cbf0165074acbe963842e86
Radu Stefan <radu.stefan@dynatrace.com>
2022-08-26 09:47:41 +0100

    added calls for extensions and monitoring configurations

--- src/dynatrace-api/interfaces/extensions.ts
+++ src/dynatrace-api/interfaces/extensions.ts
@@ -10,3 +10,24 @@ interface MinimalExtension {
   extensionName: string;
   version: string;
 }
+
+interface ExtensionMonitoringConfiguration {
+  objectId: string;
+  scope: string;
+  value: MonitoringConfigurationValue;
+}
+
+interface MonitoringConfigurationValue {
+  enabled: boolean;
+  description: string;
+  version: string;
+  featuresets?: string[];
+  vars?: any;
+  snmp?: any;
+  activationContext?: any;
+}
+
+interface ExtensionStatusDto {
+  timestamp: number;
+  status: "ERROR" | "OK" | "UNKNOWN";
+}
