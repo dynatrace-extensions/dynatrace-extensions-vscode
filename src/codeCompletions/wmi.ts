b9a13b0e7fcc38398181f305836a8bbabec80111
dlopes7 <davidribeirolopes@gmail.com>
2022-11-16 13:16:42 -0600

    Add documentation

--- src/codeCompletions/wmi.ts
+++ src/codeCompletions/wmi.ts
@@ -3,9 +3,13 @@ import * as yaml from "yaml";
 import { CachedDataProvider } from "../utils/dataCaching";
 
 export class WmiCompletionProvider implements vscode.CompletionItemProvider {
+  // The cached data provider, contains results of WMI queries previously executed by WmiCodeLens
   private readonly cachedData: CachedDataProvider;
 
   /**
+   * Provides a list of completion items based on a WMI query result
+   * The query result is previously cached in the CachedDataProvider by WmiCodeLens
+   *
    * @param cachedDataProvider a provider for cacheable data
    */
   constructor(cachedDataProvider: CachedDataProvider) {
@@ -32,7 +36,7 @@ export class WmiCompletionProvider implements vscode.CompletionItemProvider {
       .lineAt(position.line)
       .text.substring(0, position.character);
 
-    // If the line contains 'value:' the user is editing a dimension or metric
+    // If the line contains 'value:' or 'column:' the user is editing a dimension or metric
     if (line.includes("value: ") || line.includes("column:")) {
       // Find the first query definition found before this line
       const closestQueryLine = this.findClosestOcurrence(
@@ -40,31 +44,55 @@ export class WmiCompletionProvider implements vscode.CompletionItemProvider {
         position,
         document
       );
+
+      // Check that we found a query definition before proceeding
       if (closestQueryLine) {
+
+        // The line would look something like 'query: SELECT Name FROM Win32_OperatingSystem
+        // So we grab the portion after 'query:' 
         const queryString = closestQueryLine.split("query:")[1].trim();
 
         // Find out if we have a query result for this query
+        // This is only true if the query was executed by WmiCodeLens
         const cachedQueryResults =
           this.cachedData.getWmiQueryResult(queryString);
         if (!cachedQueryResults || cachedQueryResults.results.length === 0) {
           return [];
         }
-        
+
+        // We use the first result to determine the columns, all results have the same columns
         const firstResult = cachedQueryResults.results[0];
         const colummnNames = Object.keys(firstResult);
+
+        // Builds a list of completion items for each column
         return colummnNames.map((name) => {
           const suggestion = `column:${name}`;
-          const displayValue = `${suggestion} (${firstResult[name]}) <${typeof firstResult[name]}>`
-          const completionItem = new vscode.CompletionItem(displayValue, vscode.CompletionItemKind.Field);
+          const value = firstResult[name];
+
+          // This will look like 'column:Name (explorer.exe) <string>'
+          const displayText = `${suggestion} (${value}) <${typeof value}>`;
+          const completionItem = new vscode.CompletionItem(
+            displayText,
+            vscode.CompletionItemKind.Field
+          );
           completionItem.insertText = suggestion;
           completionItem.detail = `WMI result suggestion`;
-          completionItem.documentation = `${suggestion}\nValue: ${firstResult[name]}\nType: ${typeof firstResult[name]}`;
+          completionItem.documentation = new vscode.MarkdownString(
+            `Suggestion: **${suggestion}**\n\nValue: **${value}**\n\nType: **${typeof value}**`
+          );
           return completionItem;
         });
       }
     }
   }
 
+  /**
+   * Find the first occurence of a string in the document before a given position
+   * @param target The string to find
+   * @param position The position to start searching from
+   * @param document The current document
+   * @returns The line containing the target string or undefined if not found
+   */
   findClosestOcurrence(
     target: string,
     position: vscode.Position,
