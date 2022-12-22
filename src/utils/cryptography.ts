a026af8253e2df8e8eea4008639d189509402315
Radu Stefan <radu.stefan@dynatrace.com>
2022-12-22 10:44:27 +0000

    fixes #33 - fused certkey generated by dt-cli is now split

--- src/utils/cryptography.ts
+++ src/utils/cryptography.ts
@@ -42,32 +42,68 @@ export function decryptToken(token: string): string {
   return decryptedToken;
 }
 
+/**
+ * Given a merged CertKey file, this function separates and returns the RSA Key and
+ * PEM Certificate contents.
+ * @param filePath path to the merged CertKey file
+ * @returns tuple of key content and certificate content
+ */
+function getContentFromMergedFile(filePath: string): [string, string] {
+  const contetLines = fs.readFileSync(filePath).toString().split("\n");
+  const keyStart = contetLines.findIndex(l => l.includes("BEGIN RSA PRIVATE KEY"));
+  const keyEnd = contetLines.findIndex(l => l.includes("END RSA PRIVATE KEY"));
+  const certStart = contetLines.findIndex(l => l.includes("BEGIN CERTIFICATE"));
+  const certEnd = contetLines.findIndex(l => l.includes("END CERTIFICATE"));
 
+  if (keyStart === -1 || keyEnd === -1) {
+    throw new Error("CertKey file invalid: unable to find RSA Key content");
+  }
+  if (certStart === -1 || certEnd === -1) {
+    throw new Error("CertKey file invalid: unable to find Certificate content");
+  }
+
+  return [
+    contetLines.slice(keyStart, keyEnd+1).join("\n"),
+    contetLines.slice(certStart, certEnd+1).join("\n")
+  ];
+}
+
+/**
+ * Signs a given file and produces a signature file.
+ * @param inputFilePath file path of data to sign
+ * @param keyPath path to the file containing RSA Private Key
+ * @param certPath path to the file containing Certificate Key
+ * @returns signature
+ */
 export function sign(inputFilePath: string, keyPath: string, certPath: string): string {
-
   console.log(`Signing extension with certificate ${certPath} and key ${keyPath}`);
 
-  const dataToSign = fs.readFileSync(inputFilePath);
-  const keyContents = fs.readFileSync(keyPath).toString();
-  const certContents = fs.readFileSync(certPath).toString();
-  const cert = forge.pki.certificateFromPem(certContents);
+  let keyContents: string;
+  let certContents: string;
 
+  if (keyPath === certPath) {
+    [keyContents, certContents] = getContentFromMergedFile(keyPath);
+  } else {
+    keyContents = fs.readFileSync(keyPath).toString();
+    certContents = fs.readFileSync(certPath).toString();
+  }
+
+  const dataToSign = fs.readFileSync(inputFilePath);
+  const cert = forge.pki.certificateFromPem(certContents);
   const p7 = forge.pkcs7.createSignedData();
+
   p7.content = forge.util.createBuffer(dataToSign);
   p7.addCertificate(cert);
   p7.addSigner({
     key: forge.pki.privateKeyFromPem(keyContents),
     certificate: cert,
-    digestAlgorithm: forge.pki.oids.sha256
+    digestAlgorithm: forge.pki.oids.sha256,
   });
 
   p7.sign({ detached: true });
 
-  return forge.pem.encode(
-    {
-      type: 'CMS',
-      body: forge.asn1.toDer(p7.toAsn1()).getBytes()
-    }
-  );
-
+  return forge.pem.encode({
+    type: "CMS",
+    body: forge.asn1.toDer(p7.toAsn1()).getBytes(),
+  });
 }
