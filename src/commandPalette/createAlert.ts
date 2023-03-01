02186daf8d2b58d1992bf6143cf616db872bdd9d
dlopes7 <davidribeirolopes@gmail.com>
2023-03-01 08:01:29 -0600

    Add tests for alert file name generation

--- src/commandPalette/createAlert.ts
+++ src/commandPalette/createAlert.ts
@@ -162,14 +162,7 @@ function createUniqueAlertFileName(alertsDir: string, alertName: string) : strin
 
   while (true) {
     currentFileNumber++;
-    // Convert alertName to lowerCase, only allow \w and - characters
-    // It must follow the pattern [a-zA-Z0-9]+([-_./][a-zA-Z0-9]+)*
-    const alertNameForFile = alertName.toLowerCase()
-      .replace(/[^a-z0-9-]/g, "-") // Only allow a-z, 0-9 and -
-      .replace(/-+/g, "-") // Replace multiple '-' with a single '-'
-      .replace(/^-+|-+$/g, ''); // Remove leading and trailing '-'
-
-  
+    const alertNameForFile = createValidFileName(alertName);
   
     // Pad the number with zeros so the lenght is always 3
     const paddedFileNumber = currentFileNumber.toString().padStart(3, "0");
@@ -181,4 +174,15 @@ function createUniqueAlertFileName(alertsDir: string, alertName: string) : strin
     }
   }
 
+}
+
+export function createValidFileName(name: string) {
+  // Convert name to lowerCase, only allow \w and - characters
+  // It must follow the pattern [a-zA-Z0-9]+([-_./][a-zA-Z0-9]+)*
+  const nameForFile = name.toLowerCase()
+    .replace(/[^a-z0-9-]/g, "-") // Only allow a-z, 0-9 and -
+    .replace(/-+/g, "-") // Replace multiple '-' with a single '-'
+    .replace(/^-+|-+$/g, ''); // Remove leading and trailing '-'
+
+  return nameForFile;
 }
