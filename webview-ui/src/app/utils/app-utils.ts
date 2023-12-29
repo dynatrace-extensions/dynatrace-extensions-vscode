82e8541f672c50eb1ae90179068628cf79c2f071
Radu Stefan <radu.stefan@dynatrace.com>
2023-12-29 13:19:23 +0000

    add missing license/copyright info

--- webview-ui/src/app/utils/app-utils.ts
+++ webview-ui/src/app/utils/app-utils.ts
@@ -1,4 +1,20 @@
 /**
+  Copyright 2022 Dynatrace LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+/**
  * Triggers a command in VS Code from within the WebView panel.
  * This is done by encoding the args as query string parameters and clicking an
  * invisible link that can navigate to a command's function.
