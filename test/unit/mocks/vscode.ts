3746eb188fcbdf6d2c202dbf0b1768b5516e95b8
Radu Stefan <radu.stefan@dynatrace.com>
2024-01-14 18:50:36 +0000

    simulator utils tests; added copyright everywhere

--- test/unit/mocks/vscode.ts
+++ test/unit/mocks/vscode.ts
@@ -1,4 +1,45 @@
+/**
+  Copyright 2022 Dynatrace LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
 import * as vscode from "vscode";
+
+export class MockWorkspaceConfiguration implements vscode.WorkspaceConfiguration {
+  [key: string]: unknown;
+
+  get<T>(section: string): T | undefined {
+    return this[section] as T;
+  }
+
+  has(section: string): boolean {
+    return this[section] !== undefined;
+  }
+
+  inspect = jest.fn();
+
+  update<T>(section: string, value: T) {
+    this[section] = value;
+    return Promise.resolve();
+  }
+
+  constructor(state: Record<string, unknown>) {
+    Object.entries(state).forEach(([key, value]) => {
+      this[key] = value;
+    });
+  }
+}
 
 export class MockUri implements vscode.Uri {
   path;
