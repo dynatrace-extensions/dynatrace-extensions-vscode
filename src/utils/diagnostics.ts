f61b51ad70e4bd14207af5d71f694a50a8644c4a
James Kitson <james.kitson@dynatrace.com>
2024-02-12 22:53:04 -0500

    tighten up var regex

--- src/utils/diagnostics.ts
+++ src/utils/diagnostics.ts
@@ -342,7 +342,7 @@ const diagnoseVariables = async (
 
   const refVars: Array<string> = [];
   const defVars: Array<string> = [];
-  const varRegEx = /(var:[a-zA-z.-_]+)/gm;
+  const varRegEx = /: ['"]?(var:[a-zA-z.-_]+)/gm;
 
   extension.vars?.forEach(v => defVars.push(v.id));
 
