6895055c5e161688d751fe67ed15597ccc182b7a
Noah Girard <noah.girard@dynatrace.com>
2024-09-19 17:23:32 +0200

    Moved templates to different file

--- src/codeActions/activationSchema.ts
+++ src/codeActions/activationSchema.ts
@@ -18,316 +18,15 @@ import * as vscode from "vscode";
 import { getPropertyValidLines } from "../utils/jsonParsing";
 import * as logger from "../utils/logging";
 
+import {
+  humanReadableNames,
+  componentTemplates,
+  propertyTemplates,
+  numberConstraintTemplates,
+  stringConstraintTemplates,
+  preconditionTemplates,
+} from "./utils/activationSchemaTemplates";
 import { indentSnippet } from "./utils/snippetBuildingUtils";
-
-interface Map {
-  [key: string]: string;
-}
-
-const humanReadableNames: Map = {
-  boolean: "Add boolean field",
-  integer: "Add integer field",
-  object: "Add object field",
-  text: "Add text field",
-  secret: "Add secret field",
-  float: "Add float field",
-  set: "Add set field",
-  list: "Add list field",
-  color: "Add color field",
-  list_reference: "Add reference list field",
-  select: "Add dropdown field",
-  radio_button: "Add radio button field",
-  hiVictor: "Get a picture of Victor",
-  enums: "Add enumeration field",
-  credentials: "Add credential vault integration",
-  base_precondition: "Add precondition",
-  not_precondition: "Add negative precondition",
-  recursive_precondition: "Add recursive precondition",
-  range: "Add range constraints",
-  length: "Add length constraints",
-  notBlank: "Add not blank constraints",
-  trimmed: "Add trimmed constraints",
-  noWhitespace: "Add no whitespace constraints",
-};
-
-const componentTemplates: Map = {
-  enums: `"enums": {
-  "enum_name": {
-    "description": "",
-    "documentation": "",
-    "nullable": false,
-    "type": "enum",
-    "items": [
-      {
-        "displayName": "Option A",
-        "description": "Description of option A",
-        "value": "A",
-        "icon": "apple"
-      },
-      {
-        "displayName": "Option B",
-        "description": "Description of option B",
-        "value": "B",
-        "icon": "windows"
-      },
-      {
-        "displayName": "Option C",
-        "description": "Description of option C",
-        "value": "C",
-        "icon": "linux"
-      }
-    ]
-  }
-}`,
-};
-
-const propertyTemplates: Map = {
-  text: `"field_name": {
-  "displayName": "Text Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "text",
-  "default": ""
-}`,
-  secret: `"field_name": {
-"displayName": "Secret",
-"description": "Description",
-"nullable": false,
-"type": "secret",
-"default": ""
-}`,
-  boolean: `"field_name": {
-  "displayName": "Boolean Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "boolean",
-  "default": true
-}`,
-  integer: `"field_name": {
-  "displayName": "Integer Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "integer",
-  "default": 1
-}`,
-  float: `"field_name": {
-"displayName": "Float Field",
-"description": "Description",
-"nullable": false,
-"type": "float",
-"default": 1.00
-}`,
-  select: `"field_name": {
-    "displayName": "Select",
-    "description": "Description",
-    "nullable": false,
-    "type": {
-      "$ref": "#/enums/enum_name"
-    }
-}`,
-  radio_button: `"field_name": {
-  "displayName": "Radio button",
-  "description": "Description",
-  "nullable": false,
-  "type": {
-    "$ref": "#/enums/enum_name"
-  },
-  "subType": "radio
-}`,
-  credentials: `"useCredentialVault": {
-    "displayName": "Use credential vault",
-    "type": "boolean",
-    "nullable": false,
-    "default": false,
-    "maxObjects": 1
-  },
-  "credentialVaultId": {
-    "displayName": "Select vault credentials",
-    "nullable": true,
-    "type": "text",
-    "subType": "credential",
-    "referencedType": "USERNAME_PASSWORD",
-    "maxObjects": 1,
-    "precondition": {
-      "type": "EQUALS",
-      "property": "useCredentialVault",
-      "expectedValue": true
-    }
-  },
-  "username": {
-    "displayName": "User name",
-    "type": "text",
-    "nullable": false,
-    "default": "",
-    "constraints": [
-      {
-        "type": "LENGTH",
-        "minLength": 1,
-        "maxLength": 500
-      }
-    ],
-    "precondition": {
-      "type": "NOT",
-      "precondition": {
-        "type": "EQUALS",
-        "property": "useCredentialVault",
-        "expectedValue": true
-      }
-    },
-  "maxItems": 1
-  },
-  "password": {
-    "displayName": "Password",
-    "type": "secret",
-    "nullable": false,
-    "default": "",
-    "constraints": [
-      {
-        "type": "LENGTH",
-        "minLength": 1,
-        "maxLength": 500
-      }
-    ],
-    "precondition": {
-      "type": "NOT",
-      "precondition": {
-        "type": "EQUALS",
-        "property": "useCredentialVault",
-        "expectedValue": true
-      }
-    },
-  "maxItems": 1
-}`,
-  color: `"field_name": {
-"displayName": "Color",
-"description": "Description",
-"nullable": false,
-"default": "#FFEE7C",
-"type": "text",
-"subType": "color"
-}`,
-  set: `"field_name": {
-"displayName": "Set",
-"description": "Description",
-"nullable": false,
-"items": {
-  "displayName": "Text Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "text",
-  "default": ""
-},
-"type": "set"
-}`,
-  list: `"field_name": {
-"displayName": "List",
-"description": "Description",
-"nullable": false,
-"items": {
-  "displayName": "Text Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "text",
-  "default": ""
-},
-"type": "list"
-}`,
-  list_reference: `"field_name": {
-"displayName": "Object Reference",
-"description": "Description",
-"nullable": false,
-"type": "list",
-"items": {
-  "type": {
-    "$ref": "#/types/object_name"
-  }
-}
-}`,
-  object: `"object_name": {
-  "displayName": "",
-  "description": "",
-  "documentation": "",
-  "version": "",
-  "summaryPattern": "Summary: {textProp}",
-  "type": "object",
-  "properties": {
-    "textProp": {
-      "displayName": "Text Field",
-      "description": "Description",
-      "nullable": false,
-      "type": "text",
-      "default": "..."
-    }
-  }
-}`,
-  hiVictor: "😎",
-};
-
-const numberConstraintTemplates: Map = {
-  range: `"constraints": [
-    {
-      "type": "RANGE",
-      "minimum": 0,
-      "maximum": 20,
-      "customMessage": "My custom error message"
-    }
-  ]`,
-};
-
-const stringConstraintTemplates: Map = {
-  length: `"constraints": [
-  {
-    "type": "LENGTH",
-    "minLength": 1,
-    "maxLength": 500,
-    "customMessage": "My custom error message"
-  }
-]`,
-  notBlank: `"constraints": [
-  {
-    "type": "NOT_BLANK",
-    "customMessage": "My custom error message"
-  }
-]`,
-  trimmed: `"constraints": [
-  {
-    "type": "TRIMMED",
-    "customMessage": "My custom error message"
-  }
-]`,
-  noWhitespace: `"constraints": [
-  {
-    "type": "NO_WHITESPACE",
-    "customMessage": "My custom error message"
-  }
-]`,
-};
-
-const preconditionTemplates: Map = {
-  base_precondition: `"precondition": {
-  "type": "<EQUALS>/<IN>/<NULL>/<REGEX>",
-  "property": "property name",
-  "expectedValue": "value"
-}`,
-  not_precondition: `"precondition": {
-  "type": "NOT",
-  "precondition": {
-    "type": "<EQUALS>/<IN>/<NULL>/<REGEX>",
-    "property": "property name",
-    "expectedValue": "value"
-  }
-}`,
-  recursive_precondition: `"precondition": {
-  "type": "<AND/OR>",
-  "preconditions": [
-    {
-      "type": "<EQUALS>/<IN>/<NULL>/<REGEX>",
-      "property": "property name",
-      "expectedValue": "value"
-    }
-  ]
-}`,
-};
 
 /**
  * Provides singleton access to the activationSchemaActionProvider.
@@ -442,11 +141,10 @@ class activationSchemaActionProvider implements vscode.CodeActionProvider {
     propertyType: string,
   ): vscode.CodeAction[] {
     const codeActions: vscode.CodeAction[] = [];
-    let mapKey: keyof Map;
 
     if (propertyType !== "") {
       if (propertyType == "all") {
-        for (mapKey in preconditionTemplates) {
+        for (const mapKey in preconditionTemplates) {
           const action = this.createInsertAction(
             humanReadableNames[mapKey],
             preconditionTemplates[mapKey],
@@ -459,7 +157,7 @@ class activationSchemaActionProvider implements vscode.CodeActionProvider {
         }
       } else {
         if (propertyType == "number") {
-          for (mapKey in numberConstraintTemplates) {
+          for (const mapKey in numberConstraintTemplates) {
             const constraintTemplate = numberConstraintTemplates[mapKey];
             const action = this.createInsertAction(
               humanReadableNames[mapKey],
@@ -472,7 +170,7 @@ class activationSchemaActionProvider implements vscode.CodeActionProvider {
             }
           }
         } else if (propertyType == "string") {
-          for (mapKey in stringConstraintTemplates) {
+          for (const mapKey in stringConstraintTemplates) {
             const constraintTemplate = stringConstraintTemplates[mapKey];
             const action = this.createInsertAction(
               humanReadableNames[mapKey],
@@ -499,7 +197,7 @@ class activationSchemaActionProvider implements vscode.CodeActionProvider {
             codeActions.push(action);
           }
         } else {
-          for (mapKey in propertyTemplates) {
+          for (const mapKey in propertyTemplates) {
             const propertyTemplate = propertyTemplates[mapKey];
             const action = this.createInsertAction(
               humanReadableNames[mapKey],
@@ -513,7 +211,7 @@ class activationSchemaActionProvider implements vscode.CodeActionProvider {
           }
         }
       } else {
-        for (mapKey in componentTemplates) {
+        for (const mapKey in componentTemplates) {
           const componentTemplate = componentTemplates[mapKey];
           const action = this.createInsertAction(
             humanReadableNames[mapKey],
