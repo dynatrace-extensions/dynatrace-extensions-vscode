55aac4863db134a90e630beb645c667f19e41853
Balbuena <victor.balbuena@dynatrace.com>
2024-09-19 13:40:12 +0200

    fix comma issue

--- src/codeActions/activationSchema.ts
+++ src/codeActions/activationSchema.ts
@@ -15,7 +15,7 @@
  */
 
 import * as vscode from "vscode";
-import { getPropertyValidLines, getComponentValidLines } from "../utils/jsonParsing";
+import { getPropertyValidLines } from "../utils/jsonParsing";
 import * as logger from "../utils/logging";
 
 import { indentSnippet } from "./utils/snippetBuildingUtils";
@@ -24,51 +24,217 @@ interface FieldMap {
   [key: string]: string;
 }
 
+const humanReadableNames: FieldMap = {
+  boolean: "Add boolean field",
+  integer: "Add integer field",
+  object: "Add object field",
+  text: "Add text field",
+  secret: "Add secret field",
+  float: "Add float field",
+  set: "Add set field",
+  list: "Add list field",
+  color: "Add color field",
+  list_reference: "Add reference list field",
+  select: "Add dropdown field",
+  radio_button: "Add radio button field",
+  hiVictor: "Get a picture of Victor",
+  enums: "Add enumeration field",
+  credentials: "Add credential vault integration",
+};
+
 const componentTemplates: FieldMap = {
   enums: `"enums": {
-    "options": {
-      "description": "",
-      "documentation": "",
-      "nullable": false,
-      "type": "enum",
-      "items": [
-        {
-          "displayName": "Option A",
-          "description": "Description of option A",
-          "value": "A",
-          "icon": "apple"
-        },
-        {
-          "displayName": "Option B",
-          "description": "Description of option B",
-          "value": "B",
-          "icon": "windows"
-        },
-        {
-          "displayName": "Option C",
-          "description": "Description of option C",
-          "value": "C",
-          "icon": "linux"
-        }
-      ]
-    }
-  },`,
+  "enum_name": {
+    "description": "",
+    "documentation": "",
+    "nullable": false,
+    "type": "enum",
+    "items": [
+      {
+        "displayName": "Option A",
+        "description": "Description of option A",
+        "value": "A",
+        "icon": "apple"
+      },
+      {
+        "displayName": "Option B",
+        "description": "Description of option B",
+        "value": "B",
+        "icon": "windows"
+      },
+      {
+        "displayName": "Option C",
+        "description": "Description of option C",
+        "value": "C",
+        "icon": "linux"
+      }
+    ]
+  }
+}`,
 };
 
 const propertyTemplates: FieldMap = {
+  text: `"field_name": {
+  "displayName": "Text Field",
+  "description": "Description",
+  "nullable": false,
+  "type": "text",
+  "default": ""
+}`,
+  secret: `"field_name": {
+"displayName": "Secret",
+"description": "Description",
+"nullable": false,
+"type": "secret",
+"default": ""
+}`,
   boolean: `"field_name": {
   "displayName": "Boolean Field",
   "description": "Description",
   "nullable": false,
-  "type": "boolean"
-},`,
+  "type": "boolean",
+  "default": true
+}`,
   integer: `"field_name": {
   "displayName": "Integer Field",
   "description": "Description",
   "nullable": false,
-  "type": "integer"
-},`,
-  object: `"typeProp": {
+  "type": "integer",
+  "default": 1
+}`,
+  float: `"field_name": {
+"displayName": "Float Field",
+"description": "Description",
+"nullable": false,
+"type": "float",
+"default": 1.00
+}`,
+  select: `"field_name": {
+    "displayName": "Select",
+    "description": "Description",
+    "nullable": false,
+    "type": {
+      "$ref": "#/enums/enum_name"
+    }
+}`,
+  radio_button: `"field_name": {
+  "displayName": "Radio button",
+  "description": "Description",
+  "nullable": false,
+  "type": {
+    "$ref": "#/enums/enum_name"
+  },
+  "subType": "radio
+}`,
+  credentials: `"useCredentialVault": {
+    "displayName": "Use credential vault",
+    "type": "boolean",
+    "nullable": false,
+    "default": false,
+    "maxObjects": 1
+  },
+  "credentialVaultId": {
+    "displayName": "Select vault credentials",
+    "nullable": true,
+    "type": "text",
+    "subType": "credential",
+    "referencedType": "USERNAME_PASSWORD",
+    "maxObjects": 1,
+    "precondition": {
+      "type": "EQUALS",
+      "property": "useCredentialVault",
+      "expectedValue": true
+    }
+  },
+  "username": {
+    "displayName": "User name",
+    "type": "text",
+    "nullable": false,
+    "default": "",
+    "constraints": [
+      {
+        "type": "LENGTH",
+        "minLength": 1,
+        "maxLength": 500
+      }
+    ],
+    "precondition": {
+      "type": "NOT",
+      "precondition": {
+        "type": "EQUALS",
+        "property": "useCredentialVault",
+        "expectedValue": true
+      }
+    },
+  "maxItems": 1
+  },
+  "password": {
+    "displayName": "Password",
+    "type": "secret",
+    "nullable": false,
+    "default": "",
+    "constraints": [
+      {
+        "type": "LENGTH",
+        "minLength": 1,
+        "maxLength": 500
+      }
+    ],
+    "precondition": {
+      "type": "NOT",
+      "precondition": {
+        "type": "EQUALS",
+        "property": "useCredentialVault",
+        "expectedValue": true
+      }
+    },
+  "maxItems": 1
+}`,
+  color: `"field_name": {
+"displayName": "Color",
+"description": "Description",
+"nullable": false,
+"type": "text",
+"subType": "color"
+}`,
+  set: `"field_name": {
+"displayName": "Set",
+"description": "Description",
+"nullable": false,
+"items": {
+  "displayName": "Text Field",
+  "description": "Description",
+  "nullable": false,
+  "type": "text",
+  "default": ""
+},
+"type": "set"
+}`,
+  list: `"field_name": {
+"displayName": "List",
+"description": "Description",
+"nullable": false,
+"items": {
+  "displayName": "Text Field",
+  "description": "Description",
+  "nullable": false,
+  "type": "text",
+  "default": ""
+},
+"type": "list"
+}`,
+  list_reference: `"field_name": {
+"displayName": "Object Reference",
+"description": "Description",
+"nullable": false,
+"type": "list",
+"items": {
+  "type": {
+    "$ref": "#/types/object_name"
+  }
+}
+}`,
+  object: `"object_name": {
   "displayName": "",
   "description": "",
   "documentation": "",
@@ -85,91 +251,28 @@ const propertyTemplates: FieldMap = {
     }
   }
 }`,
-  text: `"field_name": {
-  "displayName": "Text Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "text"
-},`,
   hiVictor: "😎",
-  secret: `"field_name": {
-  "displayName": "Secret",
-  "description": "Description",
-  "nullable": false,
-  "type": "secret",
-  "default": "***123***"
-},`,
-  float: `"field_name": {
-  "displayName": "Integer Field",
-  "description": "Description",
-  "nullable": false,
-  "type": "float"
-},`,
-  set: `"field_name": {
-  "displayName": "Set",
-  "description": "Description",
-  "nullable": false,
-  "items": {
-    "displayName": "Text Field",
-    "description": "Description",
-    "nullable": false,
-    "type": "text",
-    "default": ""
-  },
-  "type": "set"
-}`,
-  list: `"props": {
-  "displayName": "List",
-  "description": "Description",
-  "nullable": false,
-  "items": {
-    "displayName": "Text Field",
-    "description": "Description",
-    "nullable": false,
-    "type": "text",
-    "default": ""
-  },
-  "type": "list"
-}`,
-  color: `"field_name": {
-  "displayName": "Color",
-  "description": "Description",
-  "nullable": false,
-  "type": "text",
-  "subType": "color"
-}`,
-  reference: `"props": {
-  "displayName": "Object Reference",
-  "description": "Description",
-  "nullable": false,
-  "type": "set",
-  "items": {
-    "type": {
-      "$ref": "#/types/typeProp"
-    }
-  }
-}`,
 };
 
 /**
- * Provides singleton access to the PrometheusActionProvider.
+ * Provides singleton access to the activationSchemaActionProvider.
  */
 export const getActivationSchemaActionProvider = (() => {
-  let instance: PrometheusActionProvider | undefined;
+  let instance: activationSchemaActionProvider | undefined;
 
   return () => {
-    instance = instance === undefined ? new PrometheusActionProvider() : instance;
+    instance = instance === undefined ? new activationSchemaActionProvider() : instance;
     return instance;
   };
 })();
 
 /**
- * Provider for Code Actions that work with scraped Prometheus data to automatically
+ * Provider for Code Actions that work with scraped activationSchema data to automatically
  * insert it in the Extension yaml.
  */
-class PrometheusActionProvider implements vscode.CodeActionProvider {
+class activationSchemaActionProvider implements vscode.CodeActionProvider {
   /**
-   * Provides the Code Actions that insert details based on Prometheus scraped data.
+   * Provides the Code Actions that insert details based on activationSchema scraped data.
    * @param document document that activated the provider
    * @param range range that activated the provider
    * @param context Code Action context
@@ -183,19 +286,18 @@ class PrometheusActionProvider implements vscode.CodeActionProvider {
     const codeActions: vscode.CodeAction[] = [];
 
     const lineIndex = document.lineAt(range.start.line).lineNumber;
-    const lineList = getPropertyValidLines(document.getText());
-    const lineList2 = getComponentValidLines(document.getText());
-
-    logger.info("Full list:");
-    logger.info(lineList);
-    logger.info(lineList2);
+    const [lineList, typeLineList, enumLineList] = getPropertyValidLines(document.getText());
 
     if (lineList.includes(lineIndex)) {
-      codeActions.push(...this.createMetadataInsertions(document, range, true));
+      codeActions.push(...this.createMetadataInsertions(document, range, true, false));
     }
 
-    if (lineList2.includes(lineIndex)) {
-      codeActions.push(...this.createMetadataInsertions(document, range, false));
+    if (typeLineList.includes(lineIndex)) {
+      codeActions.push(...this.createMetadataInsertions(document, range, true, true));
+    }
+
+    if (enumLineList.includes(lineIndex)) {
+      codeActions.push(...this.createMetadataInsertions(document, range, false, false));
     }
 
     return codeActions;
@@ -215,19 +317,29 @@ class PrometheusActionProvider implements vscode.CodeActionProvider {
     document: vscode.TextDocument,
     range: vscode.Range,
   ): vscode.CodeAction | undefined {
+    const indentMatch = /[^ ]/i.exec(document.lineAt(range.start.line).text);
     const bracketMatch = /[{}]/i.exec(document.lineAt(range.start.line).text);
-    if (bracketMatch) {
-      const indent = bracketMatch.index;
-      const insertPosition = new vscode.Position(range.start.line + 1, 0);
+    const [preComma, postComma] = this.checkCommaPosition(document, range);
+    if (bracketMatch && indentMatch) {
+      const indent = indentMatch.index - 2;
+      const insertPosition = new vscode.Position(
+        range.start.line,
+        document.lineAt(range.start.line).text.length,
+      );
       const action = new vscode.CodeAction(actionName, vscode.CodeActionKind.QuickFix);
       action.edit = new vscode.WorkspaceEdit();
-      action.edit.insert(document.uri, insertPosition, indentSnippet(textToInsert, indent));
+      const indentedSnippet = indentSnippet(textToInsert, indent);
+      const insertSnippet =
+        preComma +
+        indentSnippet(textToInsert, indent).substring(0, indentedSnippet.length - 1) +
+        postComma;
+      action.edit.insert(document.uri, insertPosition, insertSnippet);
       return action;
     }
   }
 
   /**
-   * Creates Code Actions for inserting metric metadata based on scraped Prometheus data.
+   * Creates Code Actions for inserting metric metadata based on scraped activationSchema data.
    * Metrics are filtered to only match the ones added in the datasource (not all scraped) and also
    * ones that don't already have metadata defined (so we don't duplicate).
    * @param document the document that triggered the action provider
@@ -239,26 +351,58 @@ class PrometheusActionProvider implements vscode.CodeActionProvider {
     document: vscode.TextDocument,
     range: vscode.Range,
     property: boolean,
+    onlyObject: boolean,
   ): vscode.CodeAction[] {
     const codeActions: vscode.CodeAction[] = [];
     let fieldType: keyof FieldMap;
     if (property) {
-      for (fieldType in propertyTemplates) {
-        const propertyTemplate = propertyTemplates[fieldType];
-        const action = this.createInsertAction(fieldType, propertyTemplate, document, range);
+      if (onlyObject) {
+        const action = this.createInsertAction(
+          "Add object field",
+          propertyTemplates.object,
+          document,
+          range,
+        );
         if (action) {
           codeActions.push(action);
+        }
+      } else {
+        for (fieldType in propertyTemplates) {
+          const propertyTemplate = propertyTemplates[fieldType];
+          const action = this.createInsertAction(
+            humanReadableNames[fieldType],
+            propertyTemplate,
+            document,
+            range,
+          );
+          if (action) {
+            codeActions.push(action);
+          }
         }
       }
     } else {
       for (fieldType in componentTemplates) {
         const componentTemplate = componentTemplates[fieldType];
-        const action = this.createInsertAction(fieldType, componentTemplate, document, range);
+        const action = this.createInsertAction(
+          humanReadableNames[fieldType],
+          componentTemplate,
+          document,
+          range,
+        );
         if (action) {
           codeActions.push(action);
         }
       }
     }
     return codeActions;
+  }
+
+  private checkCommaPosition(document: vscode.TextDocument, range: vscode.Range): [string, string] {
+    const precommaIndex = /[,{]/i.exec(document.lineAt(range.start.line).text);
+    if (precommaIndex) {
+      return ["\n", ","];
+    } else {
+      return [",\n", ""];
+    }
   }
 }
