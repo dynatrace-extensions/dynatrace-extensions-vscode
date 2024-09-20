42bb13df4595e8867e68098387c25deaa902b295
Noah Girard <noah.girard@dynatrace.com>
2024-09-20 10:16:39 +0200

    Add number metadata

--- src/codeActions/utils/activationSchemaTemplates.ts
+++ src/codeActions/utils/activationSchemaTemplates.ts
@@ -18,6 +18,7 @@ export const humanReadableNames: Record<string, string> = {
   not_precondition: "Add negative precondition",
   recursive_precondition: "Add recursive precondition",
   range: "Add range constraints",
+  numberMetadata: "Add metadata",
   length: "Add length constraints",
   notBlank: "Add not blank constraints",
   trimmed: "Add trimmed constraints",
@@ -186,6 +187,34 @@ export const propertyTemplates: Record<string, string> = {
   "type": "text",
   "subType": "color"
 }`,
+  zonedDate: `"field_name": {
+  "displayName": "Zoned Date Time",
+  "description": "Description",
+  "nullable": false,
+  "default": "2020-01-01T00:00:00+01:00",
+  "type": "zoned_date_time"
+}`,
+  localDate: `"field_name": {
+  "displayName": "Local date",
+  "description": "Description",
+  "nullable": false,
+  "default": "1970-01-01",
+  "type": "local_date"
+}`,
+  localDatetime: `"field_name": {
+  "displayName": "Local Date Time",
+  "description": "Description",
+  "nullable": false,
+  "default": "2020-01-01T00:00:00",
+  "type": "local_date_time"
+}`,
+  localTime: `"field_name": {
+  "displayName": "Local Time",
+  "description": "Description",
+  "nullable": false,
+  "default": "12:00:00",
+  "type": "local_time"
+}`,
   set: `"field_name": {
   "displayName": "Set",
   "description": "Description",
@@ -297,34 +326,6 @@ export const propertyTemplates: Record<string, string> = {
     "$ref": "#/types/typeProp"
   }
 }`,
-  zonedDate: `"field_name": {
-  "displayName": "Zoned Date Time",
-  "description": "Description",
-  "nullable": false,
-  "default": "2020-01-01T00:00:00+01:00",
-  "type": "zoned_date_time"
-}`,
-  localDate: `"field_name": {
-  "displayName": "Local date",
-  "description": "Description",
-  "nullable": false,
-  "default": "1970-01-01",
-  "type": "local_date"
-}`,
-  localDatetime: `"field_name": {
-  "displayName": "Local Date Time",
-  "description": "Description",
-  "nullable": false,
-  "default": "2020-01-01T00:00:00",
-  "type": "local_date_time"
-}`,
-  localTime: `"field_name": {
-  "displayName": "Local Time",
-  "description": "Description",
-  "nullable": false,
-  "default": "12:00:00",
-  "type": "local_time"
-}`,
   hiVictor: "😎",
 };
 
@@ -337,6 +338,10 @@ export const numberConstraintTemplates: Record<string, string> = {
       "customMessage": "My custom error message"
     }
   ]`,
+  numberMetadata: `"metadata": {
+  "suffix": "",
+  "prefix": ""
+}`,
 };
 
 export const stringConstraintTemplates: Record<string, string> = {
